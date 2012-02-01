21c3f0d044723ccf2827dd3cb68a2ea1bd392aec
Paul Batum <paul.batum@gmail.com>
2012-02-01 09:53:40 -0800

    Added a simple WCF NetHttpBinding example

---
+++ WCFNetHttpClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFNetHttpClient.ServiceReference1;
+
+namespace WCFNetHttpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new InstanceContext(new QuoteHandler());
+            StockQuoteClient client = new StockQuoteClient(context);
+
+            client.StartSendingQuotes();
+            Console.ReadLine();
+        }
+    }
+
+    public class QuoteHandler : StockQuoteCallback
+    {
+
+        public void SendQuote(string code, double value)
+        {
+            Console.WriteLine("{0}: {1}", code, value);
+        }
+
+        public async Task SendQuoteAsync(string code, double value)
+        {
+            Console.WriteLine("{0}: {1} (async)", code, value);
+        }
+    }
+}
---
+++ WCFNetHttpServer/StockQuote.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFNetHttpServer
+{
+    [ServiceContract(CallbackContract = typeof(IStockQuoteCallback))]
+    public class StockQuote
+    {
+        [OperationContract(IsOneWay = true)]
+        public async Task StartSendingQuotes()
+        {
+            var callback = OperationContext.Current.GetCallbackChannel<IStockQuoteCallback>();
+            var random = new Random();
+            double price = 29.00;
+
+            while (true)
+            {
+                await callback.SendQuote("MSFT", price);
+                price += random.NextDouble();
+                await Task.Delay(1000);
+            }
+        }
+
+    }
+
+    [ServiceContract]
+    public interface IStockQuoteCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        Task SendQuote(string code, double value);
+    }
+}
