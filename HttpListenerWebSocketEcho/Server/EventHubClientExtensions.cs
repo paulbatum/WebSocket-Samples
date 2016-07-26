5bcfceb6a8deca0fe6f0cc4a02fb4ca1f071a101
Jon Seaman <jonmseaman@live.com>
2016-07-26 15:22:50 -0700

    Added EventHubClientExtensions

---
+++ HttpListenerWebSocketEcho/Server/EventHubClientExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+
+namespace Server
+{
+    public static class EventHubClientExtensions
+    {
+        public static void SendObject(this EventHubClient client, object toSend)
+        {
+            // Serialize
+            var serializedString = JsonConvert.SerializeObject(toSend);
+            // Make eventData
+            var ed = new EventData(Encoding.UTF8.GetBytes(serializedString));
+            // Send via client
+            client.Send(ed);
+        }
+        public static async Task SendObjectAsync(this EventHubClient client, object toSend)
+        {
+            // Serialize
+            var serializedString = JsonConvert.SerializeObject(toSend);
+            // Make eventData
+            var ed = new EventData(Encoding.UTF8.GetBytes(serializedString));
+            // Send via client
+            await client.SendAsync(ed);
+        }
+
+        public static void SendBatchObjects(this EventHubClient client, IEnumerable<object> toSend)
+        {
+            // Serialize and make event data
+            var edList = ConvertBatchToEventData(toSend);
+            // Send via client
+            client.SendBatch(edList);
+        }
+        public static async Task SendBatchObjectsAsync(this EventHubClient client, IEnumerable<object> toSend)
+        {
+            // Serialize and make event data
+            var edList = ConvertBatchToEventData(toSend);
+            // Send via client
+            await client.SendBatchAsync(edList);
+        }
+        private static List<EventData> ConvertBatchToEventData(IEnumerable<object> list)
+        {
+            // Initialize list to capacity = #objects in batch.
+            var edList = new List<EventData>(list.Count());
+            foreach (object obj in list)
+            {
+                edList.Add(new EventData(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj))));
+            }
+            return edList;
+        }
+
+    }
+}
+
